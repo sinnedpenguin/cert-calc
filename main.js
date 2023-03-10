b80f2229611ac08aad4c38361ccf12bad86a3f89
sinnedpenguin <121731810+sinnedpenguin@users.noreply.github.com>
2023-03-10 22:23:20 +0800

    Fix bug
    // BUG: Displayed saved data not being listed in chronological order // Being displayed in order they were stored in the local storage
    // SOLUTION: Modified the displayedSavedData() to first extract all the data from the local storage, sort them by date, and then display them in the sorted order (Line 52)
    
    // BUG: Empty input boxes returns NaN // parseInt function returns NaN since it's unable to parse the input as an integer
    // SOLUTION: Added Number.isInteger method to displayedSavedData() to check whether each input value can be parsed as an integer. If it can, the value is added to the total variable, otherwise it is ignored (Line 52)

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// BUG: Displayed saved data not being listed in chronological order // Being displayed in order they were stored in the local storage
+// SOLUTION: Modified the displayedSavedData() to first extract all the data from the local storage, sort them by date, and then display them in the sorted order (Line 52)
+
+// BUG: Empty input boxes returns NaN // parseInt function returns NaN since it's unable to parse the input as an integer
+// SOLUTION: Added Number.isInteger method to displayedSavedData() to check whether each input value can be parsed as an integer. If it can, the value is added to the total variable, otherwise it is ignored (Line 52)
+
 document.addEventListener('DOMContentLoaded', function() {
   let data = {
     total: 0,
@@ -34,57 +40,85 @@ document.addEventListener('DOMContentLoaded', function() {
       ir: irInput.value,
       au: auInput.value,
       current: current.textContent,
-      date: new Date().toLocaleString()
+      date: new Date().toISOString()
     };
-
-    localStorage.setItem(data.date, JSON.stringify(data));
+  
+    let savedData = JSON.parse(localStorage.getItem('savedData')) || [];
+    savedData.push(data);
+    localStorage.setItem('savedData', JSON.stringify(savedData));
     displaySavedData();
   }
-
+  
   function displaySavedData() {
     let parent = document.querySelector('.saved-data');
     parent.innerHTML = '';
   
-    for (let i = 0; i < localStorage.length; i++) {
-      let key = localStorage.key(i);
-      let data = JSON.parse(localStorage.getItem(key));
+    let savedData = JSON.parse(localStorage.getItem('savedData')) || [];
+    savedData.sort(function(a, b) {
+      return new Date(a.date) - new Date(b.date);
+    });
+  
+    for (let i = 0; i < savedData.length; i++) {
+      let data = savedData[i];
   
       let div = document.createElement('div');
       let date = new Date(data.date).toLocaleDateString();
       let uk = data.uk;
       let ir = data.ir;
       let au = data.au;
-      let total = parseInt(data.uk) + parseInt(data.ir) + parseInt(data.au);
+      let total = 0;
+  
+      if (Number.isInteger(parseInt(uk))) {
+        total += parseInt(uk);
+      }
+  
+      if (Number.isInteger(parseInt(ir))) {
+        total += parseInt(ir);
+      }
+  
+      if (Number.isInteger(parseInt(au))) {
+        total += parseInt(au);
+      }
   
       div.textContent = `${date}. UK: ${uk}, IR: ${ir}, AU: ${au}, Total: ${total} `;
   
       let button = document.createElement('button');
       button.textContent = 'X';
-      button.dataset.key = key; 
       button.addEventListener('click', function() {
-        localStorage.removeItem(this.dataset.key);
+        savedData.splice(i, 1);
+        localStorage.setItem('savedData', JSON.stringify(savedData));
         displaySavedData();
       });
   
       div.appendChild(button);
       parent.appendChild(div);
-
-      let totalSaved = document.querySelector('.total');
-      let totalUK = 0;
-      let totalIR = 0;
-      let totalAU = 0;
-      for (let i = 0; i < localStorage.length; i++) {
-        let data = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    }
+  
+    let totalSaved = document.querySelector('.total');
+    let totalUK = 0;
+    let totalIR = 0;
+    let totalAU = 0;
+  
+    for (let i = 0; i < savedData.length; i++) {
+      let data = savedData[i];
+  
+      if (Number.isInteger(parseInt(data.uk))) {
         totalUK += parseInt(data.uk);
+      }
+  
+      if (Number.isInteger(parseInt(data.ir))) {
         totalIR += parseInt(data.ir);
+      }
+  
+      if (Number.isInteger(parseInt(data.au))) {
         totalAU += parseInt(data.au);
-        let totalAll = parseInt(totalUK) + parseInt(totalIR) + parseInt(totalAU);
-        let computed = (parseInt(totalUK) + parseInt(totalIR) + parseInt(totalAU)) * 25;
-        totalSaved.textContent = `UK: ${totalUK}, IR: ${totalIR}, AU: ${totalAU}, Total: ${totalAll}, Computed: ₱${computed.toLocaleString()}`;
-
       }
     }
-  };
+  
+    let totalAll = totalUK + totalIR + totalAU;
+    let computed = totalAll * 25;
+    totalSaved.textContent = `UK: ${totalUK}, IR: ${totalIR}, AU: ${totalAU}, Total: ${totalAll}, Computed: ₱${computed.toLocaleString()}`;
+  }  
 
   document.querySelector('.save-button').addEventListener('click', save);
   document.querySelectorAll('.inputs input').forEach(input => {
